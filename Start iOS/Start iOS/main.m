977c1ce99f59b1f35a354c01309e37dbdffbd134
DiegoIsRoving <10865601@qq.com>
2017-09-29 16:02:27 +0800

    commit the base code

---
+++ Start iOS/Start iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Start iOS
+//
+//  Created by stev on 2017/9/17.
+//  Copyright © 2017年 stev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
