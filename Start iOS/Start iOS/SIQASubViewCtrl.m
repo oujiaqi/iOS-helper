0e28807dbbce243a23ea10e7a84f2d5308569d3f
oujiaqi <471884121@qq.com>
2017-11-10 20:40:45 +0800

    delete unuseful file

--- Start iOS/Start iOS/SIQASubViewCtrl.m
+++ Start iOS/Start iOS/SIQASubViewCtrl.m
@@ -7,6 +7,7 @@
 //
 
 #import "SIQASubViewCtrl.h"
+#import "QADetailViewController.h"
 
 @interface SIQASubViewCtrl ()
 
@@ -16,16 +17,100 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 100;
+ 
+    
+    
+    
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];    // Do any additional setup after loading the view.
     
     //self.backgroundImageView.image = [UIImage imageNamed:self.imageFile];
     self.titleLable.text = self.titleText;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
 }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or   cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    
+    //    UIFont *font = [UIFont systemFontOfSize:15];
+    //    CGSize size = [[item description] sizeWithFont:font constrainedToSize:CGSizeMake(320,1000) lineBreakMode:NSLineBreakByWordWrapping];
+    //    cell.textLabel.frame = CGRectMake(cell.textLabel.frame.origin.x, cell.textLabel.frame.origin.y, 320, size.height);
+    //    cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    
+    
+    cell.textLabel.numberOfLines = 0;
+    
+    if(indexPath.row == 0){
+        if(_titleText==@"OC")
+            cell.textLabel.text=@"OC问题一";
+        else if(_titleText==@"Swift")
+            cell.textLabel.text=@"Swift问题一";
+        else if(_titleText==@"iOS")
+            cell.textLabel.text=@"iOS问题一";
+        else if(_titleText==@"Other")
+            cell.textLabel.text=@"Other问题一";
+    }else if(indexPath.row == 1){
+        if(_titleText==@"OC")
+            cell.textLabel.text=@"OC问题二";
+        else if(_titleText==@"Swift")
+            cell.textLabel.text=@"Swift问题二";
+        else if(_titleText==@"iOS")
+            cell.textLabel.text=@"iOS问题二";
+        else if(_titleText==@"Other")
+            cell.textLabel.text=@"Other问题二";
+    }else if(indexPath.row == 2){
+        if(_titleText==@"Swift")
+            cell.textLabel.text=@"Swift问题三";
+        else if(_titleText==@"iOS")
+            cell.textLabel.text=@"iOS问题三";
+        else if(_titleText==@"Other")
+            cell.textLabel.text=@"Other问题三";
+    }
+    
+    //cell.textLabel.text = _titleText;
+    
+    //    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    QADetailViewController *detail = [self.storyboard instantiateViewControllerWithIdentifier:@"QADetailViewController"];
+    //    QADetailViewController *detail=[[QADetailViewController alloc]init];
+    detail.mQuestion=[tableView cellForRowAtIndexPath:indexPath].textLabel.text;
+    [self.navigationController pushViewController:detail animated:YES];
+    
+    
+
+    
+}
+@end
+
+
+
+
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+//}
 
 /*
 #pragma mark - Navigation
@@ -36,13 +121,13 @@
     // Pass the selected object to the new view controller.
 }
 */
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+ //   if (self) {
         // Custom initialization
-    }
-    return self;
-}
+  //  }
+ //   return self;
+//}
 
-@end
+//@end
--- new/Start iOS/Start iOS/AddViewController.h
+++
@@ -1,6 +0,0 @@
-
-#import <UIKit/UIKit.h>
-@class BNRItem;
-@interface AddViewController : UIViewController
-@property (nonatomic, strong) BNRItem *item;
-@end
